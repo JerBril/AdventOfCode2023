fa19d9da2da82bb9e761ba3a8dcb40a6bb0c466e
Jeroen Bril <jeroenbril93@gmail.com>
2023-12-19 21:45:01 +0100

    Day 3 part 2 . Can probably be improved. But it's okay for now.

--- Day3.js
+++ Day3.js
@@ -2,17 +2,18 @@ const fs = require('fs');
 const filePath = 'Day3Input.txt';
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-  console.log(countValues(data.split('\n')));
+  console.log(countValuesPart1(data.split('\n')));
+  console.log(countValuesPart2(data.split('\n')));
 });
 
-function countValues(lines)
+function countValuesPart1(lines)
 {
-    var endValue = 0;
+    let endValue = 0;
     for(var i = 0; i <lines.length; i++)
     {
-        var currentNumber = "";
-        var countCurrentNumber = false;
-        for(var l = 0; l <lines[i].length; l++)
+        let currentNumber = "";
+        let countCurrentNumber = false;
+        for(let l = 0; l <lines[i].length; l++)
         {
             if(isCharacterNumber(lines[i][l]))
             {
@@ -84,3 +85,69 @@ function isPositionValid(arr, row, col) {
 function isCharacterNumber(char) {
     return !isNaN(parseFloat(char)) && isFinite(char);
   }
+
+
+function countValuesPart2(lines)
+{
+    let endValue = 0;
+    for(let i = 0; i <lines.length; i++)
+    {
+        for(let l = 0; l <lines[i].length; l++)
+        {
+            if(lines[i][l] == "*")
+            {
+                endValue += checkNumbersAroundGear(lines,[i,l]);
+            }
+        }
+    }
+    return endValue;
+}
+
+function checkNumbersAroundGear(lines,gearPosition)
+{
+    let foundNumbers = [];
+
+    for (let x = Math.max(0, gearPosition[0] - 1); x < Math.min(gearPosition[0] + 2, lines.length); x++) {
+        for (let y = Math.max(0, gearPosition[1] - 1); y < Math.min(gearPosition[1] + 2, lines[gearPosition[0]].length); y++) {
+            if(!isNaN(parseInt(lines[x][y])))
+            {
+                let result = findFullNumber(lines[x],y);
+                y = result[1];
+                foundNumbers.push(result[0]);                
+            }
+        }
+    }
+
+    let outcome = 0;
+    if(foundNumbers.length == 2)
+    {
+        outcome = foundNumbers[0] * foundNumbers[1];
+    }
+    return outcome;
+}
+
+function findFullNumber(line,position)
+{
+    let number = "";
+    let positionMin = (position-2 < 0) ? 0 : position-2;
+    let positionMax = (position+2 > line.length) ? line.length : position+2;
+    let end = position;
+
+    for(let i = positionMin; i <= positionMax;i++)
+    {
+        if(!isNaN(line[i]))
+        {
+            number += line[i];
+            end = i; 
+        }
+        else if(i == position-1)
+        {
+            number = "";
+        }
+        else if(i == position+1)
+        {
+            break;
+        }
+    }
+    return [Number(number),end];
+}
